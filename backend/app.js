b61b4d39836fcd1a0537da4b85901e26c874dd51
Branislav Smolicek <bsmolice@redhat.com>
2019-11-30 18:04:45 +0100

    Add basic Student and Mentor schemas

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,53 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const tasks = [];
+
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+        type Task {
+            _id: ID!
+            title: String!
+            details: String!
+            link: String
+            isSolved: Boolean!
+            isBeingSolved: Boolean!
+            creator: Mentor!
+            registeredStudent: Student
+        }
+
+        input TaskInput {
+            title: String!
+            details: String!
+        }
+
+        type Student {
+            _id: ID!
+            email: String!
+            registeredTask: Task
+            wantedTask: Task
+        }
+
+        input StudentInput {
+            email: String!
+        }
+
+        type Mentor {
+            _id: ID!
+            email: String!
+            createdTasks: [Task!]
+        }
+        
+        input MentorInput {
+            email: String!
+        }
+
         type RootQuery {
-            tasks: [String!]!
+            tasks: [Task!]!
         }
 
         type RootMutation {
-            createTask(title: String): String
+            createTask(taskInput: TaskInput): Task!
         }
 
         schema {
@@ -22,11 +61,20 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         tasks: () => {
-            return ['Test 1', 'Test 2', 'Test 3'];
+            return tasks;
         },
         createTask: (args) => {
-            const taskTitle = args.title;
-            return taskTitle;
+            const task = {
+                _id: Math.random().toString(),
+                title: args.taskInput.title,
+                details: args.taskInput.details,
+                // link: args.taskInput.link,
+                isSolved: false,
+                isBeingSolved: false
+                // creator: req.userId
+            }
+            tasks.push(task);
+            return task;
         }
     },
     // interface for testing
