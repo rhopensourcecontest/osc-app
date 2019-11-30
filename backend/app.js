6ad7e52383b13a4a29aa0d4945e82f635112cb32
Branislav Smolicek <bsmolice@redhat.com>
2019-11-30 17:42:22 +0100

    Add graphql

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            tasks: [String!]!
+        }
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World');
-})
+        type RootMutation {
+            createTask(title: String): String
+        }
 
-app.listen(8000);
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        tasks: () => {
+            return ['Test 1', 'Test 2', 'Test 3'];
+        },
+        createTask: (args) => {
+            const taskTitle = args.title;
+            return taskTitle;
+        }
+    },
+    // interface for testing
+    graphiql: true
+}));
+
+app.listen(5000);
