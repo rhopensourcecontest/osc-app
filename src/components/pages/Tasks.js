fc28111ff0bd81eaf686aafd3de54af990e09b91
Branislav Smolicek <bsmolice@redhat.com>
2019-12-07 12:51:03 +0100

    Display all tasks on TasksPage

--- src/components/pages/Tasks.js
+++ src/components/pages/Tasks.js
@@ -1,11 +1,78 @@
 import React, { Component } from 'react';
 
+import './Tasks.css';
+
 class TasksPage extends Component {
+    state = {
+        tasks: []
+    };
+
+    componentDidMount() {
+        this.fetchTasks();
+    }
+
+    fetchTasks = () => {
+        const requestBody = {
+            query: `
+                query {
+                    tasks {
+                        _id
+                        title
+                        details
+                        link
+                        isSolved
+                        isBeingSolved
+                        registeredStudent{
+                            _id
+                            email
+                        }
+                        creator {
+                            _id
+                            email
+                        }
+                    }
+                }
+            `
+        };
+
+        fetch('http://localhost:5000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                if (res.status !== 200 && res.status !== 201) {
+                    throw new Error('Failed');
+                }
+                return res.json();
+            })
+            .then(resData => {
+                const tasks = resData.data.tasks;
+                this.setState({ tasks: tasks });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
     render() {
+        const taskList = this.state.tasks.map(task => {
+            return (
+                <li key={task._id} className="tasks__list-item">
+                    <b><h4>{task.title}</h4></b>
+                    {task.details}
+                </li>
+            );
+        });
         return (
-            <div className="">
+            <React.Fragment>
                 <h1>The Tasks Page</h1>
-            </div>
+                <ul className="tasks__list">
+                    {taskList}
+                </ul>
+            </React.Fragment >
         );
     }
 }
