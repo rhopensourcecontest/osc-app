62c766c19354e7989adf8379aa3798a544960b49
Branislav Smolicek <b.smolicek@gmail.com>
2020-04-30 17:41:45 +0200

    Add dynamic api locations to fetch requests

--- src/components/api-calls/Fetch.js
+++ src/components/api-calls/Fetch.js
@@ -1,10 +1,14 @@
+const API_LOCATION = process.env.NODE_ENV === 'production'
+  ? process.env.REACT_APP_REMOTE_API_LOCATION
+  : process.env.REACT_APP_LOCAL_API_LOCATION;
+
 /**
- * Fetch from http://localhost:5000/graphql without auth header
+ * Fetch from API_LOCATION without auth header
  * @param {Object} requestBody defines query
  * @returns {Promise} response
  */
 export const fetchNoAuth = (requestBody) => {
-  return fetch('http://localhost:5000/graphql', {
+  return fetch(API_LOCATION, {
     method: 'POST',
     body: JSON.stringify(requestBody),
     headers: {
@@ -20,14 +24,14 @@ export const fetchNoAuth = (requestBody) => {
 };
 
 /**
- * Fetch from http://localhost:5000/graphql with auth header
+ * Fetch from API_LOCATION with auth header
  * 
  * @param {string} token
  * @param {Object} requestBody defines query
  * @returns {Promise} response
  */
 export const fetchAuth = (token, requestBody) => {
-  return fetch('http://localhost:5000/graphql', {
+  return fetch(API_LOCATION, {
     method: 'POST',
     body: JSON.stringify(requestBody),
     headers: {
