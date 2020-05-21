71142f3408f82ef3ac667085a0763302b93103aa
Branislav Smolicek <b.smolicek@gmail.com>
2020-05-21 12:43:35 +0200

    Minor CSS changes

--- src/components/pages/Tasks/Tasks.js
+++ src/components/pages/Tasks/Tasks.js
@@ -302,19 +302,15 @@ class TasksPage extends Component {
             <p></p>
           </Modal>
         )}
-        {/* TODO */}
         {this.context.token && this.context.isAdmin && (
           <p>Admin content</p>
         )}
-        {/* TODO */}
         {this.context.token && this.context.isMentor && !this.context.isAdmin && (
           <p>Mentor content</p>
         )}
-        {/* TODO */}
         {this.context.token && !this.context.isMentor && (
           <p>Student content</p>
         )}
-        {/* TODO */}
         {!this.context.token && (
           <p>Public content</p>
         )}
